e1c2dbda430c1b15bbb11ecef2294ed8ebdeff58
Marino <marinosanjurjo.1997@gmail.com>
2026-06-08 23:36:45 +0200

    Creación bbdd posts y autores

---
+++ db_blog.sql
@@ -0,0 +1,55 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema db_blog
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema db_blog
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `db_blog` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
+USE `db_blog` ;
+
+-- -----------------------------------------------------
+-- Table `db_blog`.`autores`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_blog`.`autores` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `imagen` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `db_blog`.`posts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_blog`.`posts` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `titulo` VARCHAR(255) NOT NULL,
+  `descripcion` LONGTEXT NOT NULL,
+  `fecha de creacion` DATETIME NOT NULL DEFAULT NOW(),
+  `categoria` VARCHAR(100) NOT NULL,
+  `autor_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_posts_autores_idx` (`autor_id` ASC) VISIBLE,
+  CONSTRAINT `fk_posts_autores`
+    FOREIGN KEY (`autor_id`)
+    REFERENCES `db_blog`.`autores` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
